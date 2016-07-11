f704fac2b29dca600610b3553d74f5f1beab8445
suwoo <keystroke915@naver.com>
2016-07-11 09:28:14 +0900

    새로 갈아엎음

---
+++ 스도쿠연습/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace 스도쿠연습
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           sudoku Sudoku = new sudoku();
+            
+            int[] temp = Sudoku.board;
+
+          
+            for (int i = 0; i < temp.Length; i++)
+            {
+                if (i % 9 == 0)
+                {
+                    Console.Write("\n\n");
+                }
+                Console.Write(temp[i] + "\t");
+               
+            }
+    
+            
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+        
+    }
+
+    public class sudoku
+    {
+        int tempQ;
+        int[,,] board = new int[3, 3, 3];
+
+    }
+    
+}
