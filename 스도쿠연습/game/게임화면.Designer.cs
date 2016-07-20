d09d8d00dc2a571fae1f7834de24dac8e9dd08e1
suwoo <keystroke915@naver.com>
2016-07-20 10:31:46 +0900

    0720_1진행중

--- 스도쿠연습/game/게임화면.Designer.cs
+++ 스도쿠연습/game/게임화면.Designer.cs
@@ -28,13 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(492, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(492, 65);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 28);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "button1";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // 게임화면
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(547, 429);
+            this.ClientSize = new System.Drawing.Size(671, 468);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Name = "게임화면";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -42,5 +65,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- 스도쿠연습/game/게임화면.cs
+++ 스도쿠연습/game/게임화면.cs
@@ -40,23 +40,32 @@ namespace 스도쿠연습.game
         {
 
             InitializeComponent();
+            makegamebuttons();
         }
 
         private void makegamebuttons()
         {
+            gamebuttons = new Button[81];
+           
             for(int i = 0; i < 81; i++)
             {
                 gamebuttons[i] = new Button();
-                gamebuttons[i].Location = new Point(txtboxleftmargin + i % 9 * 38,txtboxtopmargin + i / 9 % 9 * 27);
+                gamebuttons[i].Text = i.ToString();
+                gamebuttons[i].Location = new Point(txtboxleftmargin + i % 9 * 38,txtboxtopmargin + i / 9 % 9 * 38);
                 gamebuttons[i].Name = "gamebuttons[" + i + "]";
-                gamebuttons[i].Size = new Size(32, 21);
+                gamebuttons[i].Size = new Size(32, 32);
                 gamebuttons[i].TabIndex = i;
 
                 this.Controls.Add(gamebuttons[i]);
+                gamebuttons[i].Click += (object sender, EventArgs e) =>
+                {
+                    NumPadForm numpad = new NumPadForm();
+                    numpad.Show();
+                };
+
             }
         }
-
         
-    
+
     }
 }
